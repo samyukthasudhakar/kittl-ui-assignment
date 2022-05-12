0aca9672c91914fa076c7e6d5275b4890fa0b8eb
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-05-12 20:13:28 +0530

    refactor : image files and arch component cleanup

--- src/components/SvgArch/index.js
+++ src/components/SvgArch/index.js
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import sampleText from '../../assets/sampleText-cropped1.svg'
-import updated from '../../assets/updated.svg'
+import sampleText from '../../assets/sampleText.svg'
 import './svgArch.css'
 
 export default function SvgArch(){
 
-    const [range, setRange] = useState(0)
-
     return (
         <div className="arch-container">
             <img src={sampleText} className="arch-svg"/>
-            <input id="sliderComponent" className="slider" defaultValue="0" type="range" min="0" max="100" step="5" onChange={(e)=>{setRange(e.target.value)}}/>
-            <span>Range value : {range}</span>
+            <input id="sliderComponent" className="slider" defaultValue="0" type="range" min="0" max="100" step="5" />
         </div>
 
     )

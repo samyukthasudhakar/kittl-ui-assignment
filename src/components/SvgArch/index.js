399e3fde6d6a2e8d8c362b0df0be9774f5906318
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-05-17 09:20:55 +0530

    refactor : cleaned code in svgArch component

--- src/components/SvgArch/index.js
+++ src/components/SvgArch/index.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, useState} from 'react';
+import React, { useRef, useState} from 'react';
+
 import sampleText from '../../assets/sampleText.svg'
-import { warpImage, warpImageNow } from '../../utils/warpImage';
+import { warpImage } from '../../utils/warpImage';
 import './svgArch.css'
 
 export default function SvgArch(){
@@ -8,34 +9,25 @@ export default function SvgArch(){
     const imgRef = useRef(null)
     const canvasRef = useRef(null)
     const [warpAmount, setWarpAmount ] = useState(0)
-    var warp_canvas
-    var warp_context
-    useEffect(()=>{
-        warp_canvas  = canvasRef.current
-        warp_context = warp_canvas.getContext('2d')
-    })
+
 
     function onChangeHandler(e){
         setWarpAmount(e.target.value)
-        var invert = true
-        // if(warpAmount <= 2) 
-        // {
-        //     invert = true
-        //     setWarpAmount(2 - warpAmount)
-        // }
-        var image_to_warp = new Image()
+        let image_to_warp = new Image()
         image_to_warp.onload = function () {
-            let warp_canvas = warpImage(image_to_warp, warpAmount, invert, canvasRef)
+            let warp_canvas = warpImage(image_to_warp, warpAmount, canvasRef)
             imgRef.current.src = warp_canvas
         }
         image_to_warp.src = sampleText	
     }
 
     return(
-        <div className="arch-container">
-            <img src={sampleText} ref={imgRef} className="arch-svg"/>
+        <div>
+            <div className="img-container">
+                <img src={sampleText} ref={imgRef} className="img"/>
+                <canvas id="canvas" ref={canvasRef} style={{display:"none"}}></canvas>
+            </div>
             <input className="slider" onChange={(e)=>onChangeHandler(e)} type="range" defaultValue="1" min="0" max="2" step="0.01" />
-            <canvas id="canvas" ref={canvasRef}></canvas>
         </div>
 
     )
